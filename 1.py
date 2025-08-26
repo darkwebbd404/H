5c753e434ab892efe6240dbe557500f003f29c3c
darkwebbd404 <darkwebbd404@gmail.com>
2025-08-26 23:26:36 +0600

    Update 1.py

--- 1.py
+++ 1.py
@@ -2384,7 +2384,7 @@ for thread in threads:
     
 if __name__ == "__main__":
     try:
-        client_thread = FF_CLIENT(id="3987815200", password="B17E6BB8C9046D75DE76146FD7730470BB94AE42EA37D40D5C22313689E874F6")
+        client_thread = FF_CLIENT(id="4035300235", password="A777C3E0BBBA43B3BD52CDFD4C31128A6E0247DD1808D8A7E55EEE49D64C3315")
         client_thread.start()
     except Exception as e:
         logging.error(f"Error occurred: {e}")
